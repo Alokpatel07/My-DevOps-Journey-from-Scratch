e063c642f177743ca1c815e568014a219e9f441f
Alokpatel07 <patelalok884089@gmail.com>
2025-10-15 22:46:47 +0530

    New code added

---
+++ Month 02 - Git, Github & Python/Python_for_DevOps/environment_variable.py
@@ -0,0 +1,21 @@
+import os
+
+
+# List all environment variable
+# for key, value in os.environ.items():
+#     print(f"{key} = {value}")
+
+
+
+# How to Set Environment Variables
+
+""" 1. On Windows (Command Prompt) 
+       set MY_NAME="Alok" """
+""" 2. On Windows (Power Shell)
+       $env:MY_NAME="Alok" """
+""" 3. On Linux/Mac
+       export MY_NAME="Alok" """
+
+# This command prints name which I have given to variable
+
+print(os.getenv("My_passwd"))
